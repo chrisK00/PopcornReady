6d20a8aeaeded85b27b9172b5a1fbeef79ecba60
chrisK00 <72318023+chrisK00@users.noreply.github.com>
2021-10-08 21:20:26 +0200

    service tests

--- PopcornReady.Tests/Services/TvShowsServiceTests.cs
+++ PopcornReady.Tests/Services/TvShowsServiceTests.cs
@@ -6,6 +6,7 @@ using PopcornReady.Core.Data;
 using PopcornReady.Core.Data.Entities;
 using PopcornReady.Core.Services;
 using System;
+using System.Linq;
 using System.Threading.Tasks;
 using TestSupport.EfHelpers;
 using Xunit;
@@ -14,15 +15,42 @@ namespace PopcornReady.Tests.Services
 {
     public class TvShowsServiceTests : IDisposable
     {
-        private readonly Mock<ITvShowsApiService> _mockTvShowsApiService = new();
-        private readonly Mock<ILogger<TvShowsService>> _mockLogger = new();
         private readonly DataContext _context = new(SqliteInMemory.CreateOptions<DataContext>());
+        private readonly Mock<ILogger<TvShowsService>> _mockLogger = new();
+        private readonly Mock<ITvShowsApiService> _mockTvShowsApiService = new();
         private readonly TvShowsService _sut;
 
         public TvShowsServiceTests()
         {
             _context.Database.EnsureCreated();
             _sut = new TvShowsService(_context, _mockTvShowsApiService.Object, _mockLogger.Object);
+        }
+
+        [Fact]
+        public async Task AddAsync_AddsTvShowToDatabase_If_ItDoesntExist()
+        {
+            var tvShow = CreateTvShow();
+            int userId = 1;
+
+            await _sut.AddAsync(tvShow, userId);
+
+            _context.TvShows.Count().Should().Be(1);
+        }
+
+        [Fact]
+        public async Task AddAsync_DoesNotAddUserTvShow_If_ItIsAlreadyTracked()
+        {
+            var tvShow = CreateTvShow();
+            var user = new AppUser();
+            _context.AppUsers.Add(user);
+            _context.TvShows.Add(tvShow);
+            _context.SaveChanges();
+            _context.UserTvShows.Add(new UserTvShow { TvShowId = tvShow.Id, UserId = user.Id });
+            _context.SaveChanges();
+
+            await _sut.AddAsync(tvShow, user.Id);
+
+            _context.UserTvShows.Count().Should().Be(1);
         }
 
         public void Dispose()
@@ -34,16 +62,21 @@ namespace PopcornReady.Tests.Services
         public async Task FindAsync_CallsApiService_When_TvShowDoesNotExistInDatabaseAsync()
         {
             //Arrange
-            var tvShow = new TvShow { Name = "batman" };
+            var tvShow = CreateTvShow();
             _mockTvShowsApiService.Setup(x => x.GetTvSeriesAsync(tvShow.Name)).ReturnsAsync(tvShow);
 
             //Act
-            var result = await _sut.FindAsync("batman");
+            var result = await _sut.FindAsync(tvShow.Name);
 
             //Assert
             _mockTvShowsApiService.Verify(x => x.GetTvSeriesAsync(tvShow.Name), Times.Once);
             result.Should().NotBeNull();
             result.Name.Should().Be(tvShow.Name);
+        }
+
+        private TvShow CreateTvShow()
+        {
+            return new TvShow { Name = "batman" };
         }
     }
 }
