db71b33de498df7e1fde9fbc04d84d13ed3698a9
chrisK00 <christian.khalil2000@gmail.com>
2021-10-12 13:16:40 +0200

    meep

--- PopcornReady.Core/Options/ApiOptions.cs
+++ PopcornReady.Core/Options/ApiOptions.cs
@@ -2,7 +2,7 @@
 {
     public class ApiOptions
     {
-        public const string SectionName = "Api";
+        public const string SectionName = "Apis";
         public const string EpisodateClientName = nameof(Episodate);
         public string Episodate { get; init; }
     }
